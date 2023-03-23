ed7d4727bf42d98b5a70c319ac26c229c91e4842
alscjf1329 <alscjf1329@gmail.com>
2023-03-23 23:15:03 +0900

    feat)LinkedListStack 생성

--- stack_project/LinkedListStack.cs
+++ stack_project/LinkedListStack.cs
@@ -8,11 +8,26 @@ namespace Stack
         private Node top;
         LinkedListStack(){
             linkedList = new SingleLinkedList();
+            length=0;
         }
         public void push(object data)
         {
             top=new Node(data);
+            linkedList.add(top);
+            length++;
         }
-        
+        public object pop()
+        {
+            if(linkedList.isEmpty()){
+                Console.Write("Stack is empty!!");
+				return null;
+            } 
+            length--;
+            return linkedList.remove().data;
+        }
+        public bool isEmpty()
+        {
+            return linkedList.isEmpty();
+        }
     }
 }
