a521d8cb24de398c084e128af7e6e406ed709fc2
alscjf1329 <alscjf1329@gmail.com>
2023-03-23 22:17:24 +0900

    feat) StackByArray TEST

--- stack_project/Program.cs
+++ stack_project/Program.cs
@@ -1,6 +1,22 @@
-﻿public partial class Program
+﻿using StackByArray = vs.StackByArray;
+
+
+public partial class Program
 {
     static void Main(string[] args)
     {
+        //배열로 만든 Stack
+        StackByArray stack = new StackByArray();
+        //push
+        stack.push(1);
+        stack.push("a");
+        stack.push(2);
+        stack.push("b");
+        stack.push(3);
+        stack.push("c");
+        //pop
+        int num=1;
+        while(!stack.isEmpty()) Console.WriteLine("${0}번째 pop: ${1}",num++,stack.pop());
+        
     }
 }
